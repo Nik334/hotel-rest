5dfd4f6edea27014c22f148e71a351b59b2b1451
nikhil1 <nikkumar334@gmail.com>
2024-02-26 22:57:06 +0530

    adding some pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
+import NavbarIndex from "./components/common/NavbarIndex";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/navbar"} element={<NavbarIndex/>}/>
+                <Route path="/">
+                    <Route index element={<Home/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+        // <div className="App">
+        //   <header className="App-header">
+        //     <img src={logo} className="App-logo" alt="logo" />
+        //     <p>
+        //       Edit <code>src/App.js</code> and save to reload.
+        //     </p>
+        //     <a
+        //       className="App-link"
+        //       href="https://reactjs.org"
+        //       target="_blank"
+        //       rel="noopener noreferrer"
+        //     >
+        //       Learn React
+        //     </a>
+        //   </header>
+        // </div>
+    );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import LoaderIndex from "./LoaderIndex";
+import NavbarIndex from "./common/NavbarIndex";
+import ServiceIndex from "./ServiceIndex";
+import AboutIndex from "./AboutIndex";
+
+const Home = () => {
+    return (
+        <>
+            <LoaderIndex/>
+            <NavbarIndex/>
+            <ServiceIndex/>
+            <AboutIndex/>
+        </>
+    );
+};
+
+export default Home;
